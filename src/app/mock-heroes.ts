f5180f13c70a326301b283630e266894f69bedd2
yangyann <68040550+yangyann@users.noreply.github.com>
2020-08-20 17:29:33 +0800

    another new things

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Never' },
+    { id: 12, name: 'Im' },
+    { id: 13, name: 'Find' },
+    { id: 14, name: 'Its' },
+    { id: 15, name: 'OK' },
+    { id: 16, name: 'Pick' },
+    { id: 17, name: 'Me' },
+    { id: 18, name: 'Up' },
+    { id: 19, name: 'Always' },
+    { id: 20, name: 'Mind' },
+];
